bccf16cdfed90e0e6cb7b6ddae61c73a54b03b6e
zack32527 <157523220+zack32527@users.noreply.github.com>
2024-05-08 21:22:05 -0400

    Complete

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,26 @@
 document.querySelector("#copyright").textContent = new Date().getFullYear();
 
+
+const navigationMenu = document.getElementById("navmenu");
+const navigationButton = document.getElementById("toggle-menu");
+
 // Toggle Mobile Menu
+function toggleMenu() {
+    let isVisible = navigationMenu.getAttribute("data-visible");
+
+    if(isVisible === "false") {
+        if(navigationMenu.classList.contains("active")) {
+            navigationMenu.setAttribute("data-visible", "true");
+            navigationButton.setAttribute("data-visible", "true");
+        }
+        else{
+            dataDisplay();
+            setTimeout(toggleMenu, 100);
+        }
+    }
+
+    function () {
+        setTimeout(toggleMenu, 100); 
+    }
+    
+}
